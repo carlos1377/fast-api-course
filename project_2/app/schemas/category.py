366ebeda8561d30165d62979ee95ab387b901595
carlos1377 <carloseduardoorso1@gmail.com>
2024-03-03 20:08:36 -0300

    first commit of project, created routes, models, tests and dockerfile / docker-compose of project.

---
+++ project_2/app/schemas/category.py
@@ -0,0 +1,18 @@
+import re
+from app.schemas.base import CustomBaseModel
+from pydantic import field_validator
+
+
+class Category(CustomBaseModel):
+    name: str
+    slug: str
+
+    @field_validator('slug')
+    def validate_slug(cls, value):
+        if not re.match('^([a-z]|-|_)+$', value):
+            raise ValueError('Invalid Slug')
+        return value
+
+
+class CategoryOutput(Category):
+    id: int
---
+++ project_2/app/test/schemas/test_category_schema.py
@@ -0,0 +1,33 @@
+import pytest
+from app.schemas.category import Category
+from pydantic import ValidationError
+
+
+def test_category_schema():
+    category = Category(
+        name='Roupa',
+        slug='roupa'
+    )
+
+    assert category.dict() == {
+        'name': 'Roupa',
+        'slug': 'roupa'
+    }
+
+
+def test_category_schema_invalid_slug():
+    with pytest.raises(ValueError):
+        category = Category(
+            name='Roupa',
+            slug='roupa de cama'
+        )
+    with pytest.raises(ValueError):
+        category = Category(
+            name='Roupa',
+            slug='ção'
+        )
+    with pytest.raises(ValueError):
+        category = Category(
+            name='Roupa',
+            slug='ROupa'
+        )
